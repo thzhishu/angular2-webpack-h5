0fed08eaa64c71690251f6d3290c2e459a3ac76b
Rain.xia <rain.xia@mytianhui.com>
2016-08-22 16:36:59 +0800

    fixed home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,20 +10,16 @@ import { XLarge } from './x-large';
   // where, in this case, selector is the string 'home'
   selector: 'home',  // <home></home>
   // We need to tell Angular's Dependency Injection which providers are in our app.
-  providers: [
-    Title
-  ],
+  providers: [],
   // We need to tell Angular's compiler which directives are in our template.
   // Doing so will allow Angular to attach our behavior to an element
-  directives: [
-    XLarge
-  ],
+  directives: [],
   // We need to tell Angular's compiler which custom pipes are in our template.
   pipes: [ ],
   // Our list of styles in our component. We may add more to compose many styles together
-  styleUrls: [ './home.style.scss' ],
+  styles: [ require('./home.style.scss') ],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
-  templateUrl: './home.template.html'
+  template: require('./home.template.html')
 })
 export class Home {
   // Set our default values
