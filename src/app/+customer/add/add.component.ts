83d6fe9960922c8f68fe5bbf5efff7bdf257965a
Rain.xia <rain.xia@mytianhui.com>
2016-08-24 18:33:43 +0800

    customer add page

---
+++ src/app/+customer/add/add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'cutomer-add',
+    template: require('./add.template.html'),
+    styles: [require('./add.style.scss')]
+})
+export class CustomerAdd {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Home } from './home';
 import { NoContent } from './no-content';
 
-import { Dashboard, CustomerList,BusinessList } from './+dashboard';
+import { Dashboard, CustomerList, BusinessList, CustomerAdd } from './+dashboard';
 
 import { Login, Index } from './+login';
 import { Register } from './+register';
@@ -35,7 +35,8 @@ export const ROUTES: Routes = [
           path: 'customer',
           children: [
               { path: '', redirectTo: 'list', pathMatch: 'full'},
-              { path: 'list', component: CustomerList }
+              { path: 'list', component: CustomerList },
+              { path: 'add', component: CustomerAdd }
           ]
       },
       {
